b606cb2642f538165a99f7ad21b09c0d2dbbf700
yokoro13 <yokoro.kaito.yi3@is.naist.jp>
2021-06-20 01:54:21 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,12 @@ fi
 
 # 実験時のGCEでdocekr-composeコマンドが使えなかったため、それ用のdocker imageを使用した
 # ※デーモン(-d)にしないとCIがいつまでも終わらないので注意
-docker run \
---rm -v /var/run/docker.sock:/var/run/docker.sock \
--v "$PWD:/$PWD" -w="/$PWD" \
-docker/compose:1.22.0 \
-up -d
+
+docker-compose build --build-arg GoogleClientID=$2 --build-arg GoogleClientSecret=$3
+docker-compose up -d
+
+#docker run \
+#--rm -v /var/run/docker.sock:/var/run/docker.sock \
+#-v "$PWD:/$PWD" -w="/$PWD" \
+#docker/compose:1.22.0 \
+#up -d
