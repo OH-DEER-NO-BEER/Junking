dd4cd797013284ffd29f0c8292edb4c78cc08d46
WakishiDeer <by9132@gmail.com>
2021-06-19 18:12:22 +0900

    Add Unity files

--- app/views/src/components/Body.vue
+++ app/views/src/components/Body.vue
@@ -1,12 +1,20 @@
 <template>
-<div class="small">
-    <h1>Body.vue</h1>
-    <bar-chart></bar-chart>
-    <radar-chart></radar-chart>
+<!-- at first, do 'load()' function -->
+<div class="small" onload="load();">
+  <h1>Body.vue</h1>
+  <div>
+    <v-text-field label="Room ID" placeholder="Put Room ID Here">
+    </v-text-field>
+    <v-btn dark rounded v-on:click="sendRoomID">submit</v-btn>
+      <!-- <button class="button" v-on:click="onGet">get</button> -->
+  </div>
+  <bar-chart></bar-chart>
+  <radar-chart></radar-chart>
 </div>
 </template>
 
 <script>
+import axios from 'axios';
 import BarChart from './BarChart';
 import RadarChart from './RadarChart';
 
@@ -15,6 +23,49 @@ export default {
     BarChart,
     RadarChart,
   },
+  data(){
+    // data should be write down below
+    return{
+      checkInRequest: "hoge",
+    }
+  },
+  methods:{
+    // when this component is loaded, do below function
+    onLoad(){
+      // delete former value if NOT null
+      if(this.getRoomID() != null){
+        this.removeRoomID();
+        console.log("deleted");
+      }
+      // registar 'storageChange' as event listener
+      window.addEventListener("storage", storageChange);
+      },
+    storageChange(event){
+      // PROCESS FOR SENDING TO GOLANG
+      this.sendRoomID();
+    },
+    removeRoomID(){
+      window.localStorage.removeItem('roomID');
+    },
+    storeRoomID(){
+      var inputRoomID = document.getElementById("input-room-ID");
+      window.localStorage.setItem("roomID", inputRoomID.value);
+    },
+    getRoomID(){
+      return window.localStorage.getItem("roomID");
+    },
+    sendRoomID(){
+      // send roomID to Go-server via axios
+      axios.post("/roomID", this.makeCheckInJson()).then(response =>{
+        this.response = response.data;
+      });
+    },
+    makeCheckInJson(){
+      return {
+        roomID: this.getRoomID(),
+      }
+    }
+  }
 }
 </script>
 
