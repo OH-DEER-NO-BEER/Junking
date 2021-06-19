7652cbc82c3a2326fa78262b155fbe86d6f9c887
17t2037h <17t2037h@shinshu-u.ac.jp>
2021-06-20 07:35:55 +0900

    made hubs.

--- app/controllers/checkinController.go
+++ app/controllers/checkinController.go
@@ -1,12 +1,123 @@
 package controllers
 
 import (
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/gin-gonic/gin"
 
 	"github.com/gorilla/websocket"
 )
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// connection is an middleman between the websocket connection and the hub.
+type connection struct {
+	// The websocket connection.
+	ws *websocket.Conn
+
+	// Buffered channel of outbound messages.
+	send chan []byte
+}
+
+// readPump pumps messages from the websocket connection to the hub.
+func (s subscription) readPump() {
+	c := s.conn
+	defer func() {
+		h.unregister <- s
+		c.ws.Close()
+	}()
+	c.ws.SetReadLimit(maxMessageSize)
+	c.ws.SetReadDeadline(time.Now().Add(pongWait))
+	c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, msg, err := c.ws.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		m := message{msg, s.room}
+		h.broadcast <- m
+	}
+}
+
+// write writes a message with the given message type and payload.
+func (c *connection) write(mt int, payload []byte) error {
+	c.ws.SetWriteDeadline(time.Now().Add(writeWait))
+	return c.ws.WriteMessage(mt, payload)
+}
+
+// writePump pumps messages from the hub to the websocket connection.
+func (s *subscription) writePump() {
+	c := s.conn
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.ws.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				c.write(websocket.CloseMessage, []byte{})
+				return
+			}
+			if err := c.write(websocket.TextMessage, message); err != nil {
+				return
+			}
+		case <-ticker.C:
+			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
+		}
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+type message struct {
+	data []byte
+	room string
+}
+
+type subscription struct {
+	conn *connection
+	room string
+}
+
+type Hub struct {
+	// Registered connections.
+	rooms map[string]map[*connection]bool
+
+	// Inbound messages from the connections.
+	broadcast chan message
+
+	// Register requests from the connections.
+	register chan subscription
+
+	// Unregister requests from connections.
+	unregister chan subscription
+}
 
 var clients = make(map[*websocket.Conn]bool) // 接続されるクライアント
 var broadcast = make(chan Message)           // メッセージブロードキャストチャネル
@@ -19,11 +130,24 @@ type Message struct {
 }
 
 func CheckIn(c *gin.Context) {
-	var upgrader = websocket.Upgrader{}
+	roomId := c.Param("roomId")
+	fmt.Print(roomId)
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
 	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err.Error())
+		return
 	}
+	con := &connection{send: make(chan []byte, 256), ws: ws}
+	s := subscrption{con, roomId}
+	rooomsHub.register <- s
+
+	go s.writePump()
+	go s.readPump()
+
 	// 関数が終わった際に必ずwebsocketnのコネクションを閉じる
 	defer ws.Close()
 
@@ -44,7 +168,7 @@ func CheckIn(c *gin.Context) {
 	}
 }
 
-func handleMessages() {
+func HandleMessages() {
 	for {
 		// ブロードキャストチャネルから次のメッセージを受け取る
 		msg := <-broadcast
--- app/server.go
+++ app/server.go
@@ -10,15 +10,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// func helloWorld(w http.ResponseWriter, r *http.Request) {
-// 	r.ParseForm()       //オプションを解析します。デフォルトでは解析しません。
-// 	fmt.Println(r.Form) //このデータはサーバのプリント情報に出力されます。
-// 	fmt.Println("path", r.URL.Path)
-// 	fmt.Println("scheme", r.URL.Scheme)
-// 	fmt.Println(r.Form["url_long"])
-// 	fmt.Fprintf(w, "Hello, world!") //ここでwに入るものがクライアントに出力されます。
-// }
-
 func sampleMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		name, name_err := c.Cookie("Name")
@@ -26,7 +17,6 @@ func sampleMiddleware() gin.HandlerFunc {
 		email, email_err := c.Cookie(("Email"))
 		fmt.Println("name =", name, "id =", id, ", email =", email, "name_err =", name_err, "id_err =", id_err, ", emai_err =", email_err)
 		if name_err != nil || id_err != nil || email_err != nil {
-			// c.Redirect()
 			fmt.Println("No Cookie!")
 			controllers.GetOauth2(c)
 			c.Abort()
@@ -50,39 +40,15 @@ func main() {
 	loginGroup.Use(sampleMiddleware())
 	{
 		loginGroup.GET("/", func(c *gin.Context) {
-			// session := sessions.Default(c)
-			// accessToken := session.Get("access_token")
-			// accessSecret := session.Get("access_secret")
-			// if accessToken == nil || accessSecret == nil {
-
-			// requestToken, requestSecret, _ := config.RequestToken()
-			// session.Set("request_secret", requestSecret)
-			// session.Save()
-			// c.Redirect(http.StatusFound, )
-
-			// c.HTML(http.StatusOK, "index.html", gin.H{
-			// 	"message": "Hi! here is root!",
-			// })
 			c.Redirect(http.StatusTemporaryRedirect, "/app")
 		})
-		// engine.GET("/", controllers.GetOauth2)
+
 		loginGroup.GET("/app", func(c *gin.Context) {
 			c.HTML(http.StatusOK, "index.html", gin.H{})
 		})
 
 		loginGroup.GET("/ws:roomId", controllers.CheckIn)
 	}
-
-	// engine.Static("/view", "./views")
-
-	// store := cookie.NewStore([]byte("secret"))
-	// engine.Use(sessions.Sessions("mysession", store))
-
-	// engine.StaticFS("/", http.Dir("./views/static"))
-
-	// engine.GET("/", func(c *gin.Context) {
-	// 	c.HTML(http.StatusOK, "index.html", gin.H{})
-	// })
 
 	err := engine.Run(":8080")
 	if err != nil {
