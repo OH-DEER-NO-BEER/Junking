a8c8d2b4cf84942d602634e4bb201ed271175970
yokoro13 <yokoro.kaito.yi3@is.naist.jp>
2021-06-20 03:04:02 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,5 @@ else
   cd Junking
 fi
 
-# 実験時のGCEでdocekr-composeコマンドが使えなかったため、それ用のdocker imageを使用した
-# ※デーモン(-d)にしないとCIがいつまでも終わらないので注意
-
 docker-compose build --build-arg GoogleClientID=$2 --build-arg GoogleClientSecret=$3
 docker-compose up -d
-
-#docker run \
-#--rm -v /var/run/docker.sock:/var/run/docker.sock \
-#-v "$PWD:/$PWD" -w="/$PWD" \
-#docker/compose:1.22.0 \
-#up -d
