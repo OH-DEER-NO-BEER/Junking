54d2688334d3b01e558d49ea242a6b4790288926
WakishiDeer <by9132@gmail.com>
2021-06-20 03:01:14 +0900

    Some changes for unity and JavaScript

--- app/views/src/components/Body.vue
+++ app/views/src/components/Body.vue
@@ -1,11 +1,11 @@
 <template>
 <!-- at first, do 'load()' function -->
-<div class="small" onload="load();">
+<div class="small">
   <h1>Body.vue</h1>
   <div>
-    <v-text-field label="Room ID" placeholder="Put Room ID Here">
+    <v-text-field id="input-room-ID" label="Room ID" placeholder="Put Room ID Here">
     </v-text-field>
-    <v-btn dark rounded v-on:click="sendRoomID">submit</v-btn>
+    <v-btn dark rounded>submit</v-btn>
       <!-- <button class="button" v-on:click="onGet">get</button> -->
   </div>
   <bar-chart></bar-chart>
@@ -14,7 +14,6 @@
 </template>
 
 <script>
-import axios from 'axios';
 import BarChart from './BarChart';
 import RadarChart from './RadarChart';
 
@@ -26,45 +25,11 @@ export default {
   data(){
     // data should be write down below
     return{
-      checkInRequest: "hoge",
     }
   },
+  mounted(){
+  },
   methods:{
-    // when this component is loaded, do below function
-    onLoad(){
-      // delete former value if NOT null
-      if(this.getRoomID() != null){
-        this.removeRoomID();
-        console.log("deleted");
-      }
-      // registar 'storageChange' as event listener
-      window.addEventListener("storage", storageChange);
-      },
-    storageChange(event){
-      // PROCESS FOR SENDING TO GOLANG
-      this.sendRoomID();
-    },
-    removeRoomID(){
-      window.localStorage.removeItem('roomID');
-    },
-    storeRoomID(){
-      var inputRoomID = document.getElementById("input-room-ID");
-      window.localStorage.setItem("roomID", inputRoomID.value);
-    },
-    getRoomID(){
-      return window.localStorage.getItem("roomID");
-    },
-    sendRoomID(){
-      // send roomID to Go-server via axios
-      axios.post("/roomID", this.makeCheckInJson()).then(response =>{
-        this.response = response.data;
-      });
-    },
-    makeCheckInJson(){
-      return {
-        roomID: this.getRoomID(),
-      }
-    }
   }
 }
 </script>
