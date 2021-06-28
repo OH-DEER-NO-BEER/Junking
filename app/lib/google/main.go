7451b3fde834f848ae0f9655246eb2a3037e8e29
17t2037h <17t2037h@shinshu-u.ac.jp>
2021-06-28 17:18:23 +0900

    changed  to https

--- app/lib/google/main.go
+++ app/lib/google/main.go
@@ -12,12 +12,12 @@ const (
 )
 
 func GetConnection() *oauth2.Config {
-	var protocol string
-	if os.Getenv("HostName") == "localhost" {
-		protocol = "http://"
-	} else {
-		protocol = "https://"
-	}
+	// var protocol string
+	// if os.Getenv("HostName") == "localhost" {
+	// 	protocol = "http://"
+	// } else {
+	// 	protocol = "https://"
+	// }
 
 	config := &oauth2.Config{
 		ClientID:     os.Getenv("GoogleClientID"),
@@ -26,7 +26,7 @@ func GetConnection() *oauth2.Config {
 			AuthURL:  authorizeEndpoint,
 			TokenURL: tokenEndpoint,
 		},
-		RedirectURL: protocol + os.Getenv("HostName") + ":8080/google/callback",
+		RedirectURL: "https://" + os.Getenv("HostName") + ":8080/google/callback",
 		Scopes:      []string{"openid", "email", "profile"},
 	}
 	return config
