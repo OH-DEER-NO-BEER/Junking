976854a6129c0710712780b1d9f07ea7c4406777
17t2037h <17t2037h@shinshu-u.ac.jp>
2021-07-01 02:09:19 +0900

    check the redirect url.

--- app/lib/google/main.go
+++ app/lib/google/main.go
@@ -26,7 +26,7 @@ func GetConnection() *oauth2.Config {
 			AuthURL:  authorizeEndpoint,
 			TokenURL: tokenEndpoint,
 		},
-		RedirectURL: "https://" + os.Getenv("HostName") + ":8080/google/callback",
+		RedirectURL: "https://" + os.Getenv("HostName") + ":8080",
 		Scopes:      []string{"openid", "email", "profile"},
 	}
 	return config
