c37370d1b9b8e70f90ed66dfe128eef5b458a764
17t2037h <17t2037h@shinshu-u.ac.jp>
2021-07-01 03:14:37 +0900

    resetted to https

--- app/lib/google/main.go
+++ app/lib/google/main.go
@@ -26,7 +26,7 @@ func GetConnection() *oauth2.Config {
 			AuthURL:  authorizeEndpoint,
 			TokenURL: tokenEndpoint,
 		},
-		RedirectURL: "https://" + os.Getenv("HostName") + ":8080",
+		RedirectURL: "https://" + os.Getenv("HostName") + ":8080/google/callback",
 		Scopes:      []string{"openid", "email", "profile"},
 	}
 	return config
