10baa936f3ed0b1189f117bc85ea745de5ffba80
17t2037h <17t2037h@shinshu-u.ac.jp>
2021-07-01 03:38:55 +0900

    no oatuth

--- app/server.go
+++ app/server.go
@@ -17,11 +17,11 @@ func sampleMiddleware() gin.HandlerFunc {
 		id, id_err := c.Cookie("ID")
 		email, email_err := c.Cookie(("Email"))
 		fmt.Println("name =", name, "id =", id, ", email =", email, "name_err =", name_err, "id_err =", id_err, ", emai_err =", email_err)
-		if name_err != nil || id_err != nil || email_err != nil {
-			fmt.Println("No Cookie!")
-			controllers.GetOauth2(c)
-			c.Abort()
-		}
+		// if name_err != nil || id_err != nil || email_err != nil {
+		// 	fmt.Println("No Cookie!")
+		// 	controllers.GetOauth2(c)
+		// 	c.Abort()
+		// }
 		c.Next()
 	}
 }
