61ba26adc01c327da9ab143a17e4b81446a41902
yokoro13 <yokoro.kaito.yi3@is.naist.jp>
2021-07-03 03:42:02 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,6 @@ else
   cd Junking
 fi
 
+docker-compose down -v
 docker-compose build --build-arg GoogleClientID=$2 --build-arg GoogleClientSecret=$3 --build-arg CertFile=$4 --build-arg KeyFile=$5
 docker-compose up -d
